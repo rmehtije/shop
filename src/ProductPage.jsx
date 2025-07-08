17b9ef5f07b70bdac8375c55902f7ee2e90f0741
rmehtije <rmehtije@gmail.com>
2025-07-08 20:58:01 +0300

    New category component

--- src/ProductPage.jsx
+++ src/ProductPage.jsx
@@ -2,10 +2,12 @@ import Container from "react-bootstrap/Container";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Badge from 'react-bootstrap/Badge';
+import Category from "./Category";
 
 function ProductPage() {
     return (
         <Container>
+            <Category />
             <Row>
                 <Col>
                     image
